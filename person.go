9dac3a0aca93837c37bc5adf8eda8ba063cfbc33
Mark Chadwick <m.chadwick@gns.cri.nz>
2015-08-30 23:12:12 +1200

    fixed person names

--- person.go
+++ person.go
@@ -3,7 +3,7 @@ package fdsn
 // Representation of a person's contact information.
 // A person can belong to multiple agencies and have multiple email addresses and phone numbers.
 type Person struct {
-	Name         []string      `xml:",omitempty"`
+	Names        []string      `xml:"Name,omitempty"`
 	Agencies     []string      `xml:"Agency,omitempty"`
 	Email        []Email       `xml:"Email,omitempty"`
 	PhoneNumbers []PhoneNumber `xml:"Phone,omitempty"`
